550727d35e096b717fb671364f6e90fc2b683e3a
angelmaria831 <104212477+angelmaria831@users.noreply.github.com>
2024-06-03 14:34:26 +0100

    Add files via upload

---
+++ auction.js
@@ -0,0 +1,104 @@
+import tty from 'bare-tty'
+import b4a from 'b4a'
+import readline from 'bare-readline'
+import process from 'bare-process'
+
+const readInput = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+export async function createAuction(batch, bee, core, rl, swarm) {
+
+    console.log('Creating Auction...')
+    const itemValue = { 'unit': 'USDt', 'status': 'live' }
+    const roomDetails = {}
+    rl.input.setMode(tty.constants.MODE_RAW)
+
+    console.log('Enter the Itemname,Price(comma-sepearted) : ')
+
+    readInput.on('data', async (data) => {
+
+        itemValue['name'] = data.split(',')[0]
+        itemValue['price'] = data.split(',')[1]//valid number
+        readInput.prompt()
+
+        const itemNumber = Date.now() + itemValue['name']
+        const discovery = swarm.join(core.discoveryKey)
+        const roomKey = b4a.toString(core.key, 'hex')
+
+        discovery.flushed().then(() => {
+            console.log('Room for Bidding: ', roomKey)
+
+        })
+        roomDetails['itemNumber'] = itemNumber
+        roomDetails['creator'] = b4a.toString(swarm.keyPair.publicKey, 'hex')
+        roomDetails['status'] = 'live'
+        roomDetails['highestBidName'] = 'self'
+        roomDetails['highestBidPrice'] = itemValue['price']
+
+        await batch.put(itemNumber, itemValue, { cas })
+        await batch.put(roomKey, roomDetails, { cas })
+        await batch.flush()
+        readInput.close()
+    })
+
+}
+
+export async function joinAuction(core, swarm, bee, key) {
+    swarm.join(core.discoveryKey)
+    const roomDetails = await bee.get(key)
+    console.log('Fetching Item details....', JSON.stringify(roomDetails))
+    bee.get(roomDetails.value.itemNumber).then(data => {
+        console.log(`[info] Unit: ${data.value.unit} \n Status: ${data.value.status}\n Name: ${data.value.name}\n Price: ${data.value.price}`);
+    })
+
+}
+
+export async function highestBid(message, swarm, name, bee, core, batch) {
+    const roomKey = b4a.toString(core.key, 'hex')
+    const publicKey = b4a.toString(swarm.keyPair.publicKey, 'hex')
+
+    bee.get(roomKey).then(async (roomDetails) => {
+
+        if (roomDetails.value.highestBidPrice < parseFloat(message) && publicKey.toString() == roomDetails.value.creator) {
+            const updatedData = {}
+            updatedData['itemNumber'] = roomDetails.value.itemNumber
+            updatedData['creator'] = roomDetails.value.creator
+            updatedData['status'] = roomDetails.value.status
+            updatedData['highestBidName'] = name
+            updatedData['highestBidPrice'] = parseFloat(message)
+
+            await batch.put(roomKey, updatedData, { cas })
+            await batch.flush()
+
+            let roomDetails = await bee.get(roomKey)
+            console.log('Fetching item updated details....', roomDetails.value)
+            return true
+        } else return false
+    })
+
+
+}
+export async function closeAuction(swarm, bee, core, batch) {
+    const roomKey = b4a.toString(core.key, 'hex')
+    const roomDetails = await bee.get(roomKey)
+    const publicKey = b4a.toString(swarm.keyPair.publicKey, 'hex')
+    if (publicKey.toString() === roomDetails.value.creator) {
+        roomDetails.value.status = 'Closed'
+        const itemDetails = await bee.get(roomDetails.value.itemNumber)
+        itemDetails.value.Status = 'Sold'
+
+        await batch.put(roomKey, roomDetails.value, { cas })
+        await batch.put(roomDetails.value.itemNumber, itemDetails.value, { cas })
+        await batch.close()
+        return roomDetails.value.highestBidName, roomDetails.value.highestBidPrice
+    }
+
+
+}
+
+
+function cas(prev, next) {
+    return prev.value !== next.value
+}
---
+++ index.js
@@ -0,0 +1,76 @@
+import Hyperswarm from "hyperswarm";
+import Corestore from "corestore";
+import Hyperbee from "hyperbee";
+import b4a from 'b4a'
+import readline from 'bare-readline'
+import tty from 'bare-tty'
+import process from 'bare-process'
+import { createAuction, joinAuction, highestBid, closeAuction } from './auction'
+
+const store = new Corestore(Pear.config.storage)
+
+const swarm = new Hyperswarm()
+Pear.teardown(() => swarm.destroy())
+
+
+const argVal = Pear.config.args[0]
+const core = (argVal == 'create') ? store.get({ name: 'my-auction' }) : store.get({ key: b4a.from(argVal, 'hex') })
+
+const bee = new Hyperbee(core, {
+    keyEncoding: 'utf-8',
+    valueEncoding: 'json'
+})
+
+await core.ready()
+
+const batch = bee.batch()
+
+swarm.on('connection', peer => {
+    store.replicate(peer)
+    const name = b4a.toString(peer.remotePublicKey, 'hex').substr(0, 6)
+    //Client#2 makes bid for Client#1->Pic#1 with 75 USDt
+
+    peer.on('data', message => {
+
+        if (isAlphanumeric(message)) {
+            console.log(`${name} makes bid : ${message} USDt`)
+            highestBid(parseFloat(message), swarm, name, bee, core)
+        }
+
+    })
+    peer.on('error', e => console.log(`Error : ${e}`))
+    peer.on('close', () => {
+        console.log(`[connection left] ${peer}`)
+    })
+})
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+
+rl.input.setMode(tty.constants.MODE_RAW)
+rl.on('data', line => {
+
+    if (!isNaN(line)) sendMessage(line)
+    else if (line === 'close') {
+        const { name, price } = closeAuction(bee, core, batch)
+        sendMessage(`Closing Auction..., Name: ${name}, Price : ${price}`)
+    }
+    rl.prompt()
+})
+
+if(argVal == 'create')createAuction(batch, bee, core, rl, swarm)
+    else joinAuction(core, swarm, bee, key)
+
+
+function sendMessage(message) {
+    const peers = [...swarm.connections]
+    for (const peer of peers) peer.write(message)
+}
+
+function isAlphanumeric(str) {
+    return /^[a-zA-Z0-9]+$/.test(str);
+}
